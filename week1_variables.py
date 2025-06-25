f89dc57621afe1bc65bb382f08be3068db0d51fd
Kamogelo123-tech <kmakokoe200@gmail.com>
2025-06-25 03:00:58 +0200

    Create week1_variables.py

---
+++ week1_variables.py
@@ -0,0 +1,12 @@
+# Weeek 1: Python Basics
+# Created by Kamogelo
+
+# This program stores your name and prints a welcome message
+your_name = "Kamogelo"
+print("Welcome to python, "+ your_name)
+
+
+
+
+
+
