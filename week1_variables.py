a21c4bce77a462cb880011a7b1b2eed07650904d
Kamogelo123-tech <kmakokoe200@gmail.com>
2025-06-26 14:23:37 +0200

    Update week1_variables.py

--- week1_variables.py
+++ week1_variables.py
@@ -1,12 +1,25 @@
 # Weeek 1: Python Basics
 # Created by Kamogelo
 
+
+
+# Variables
+
+
 # This program stores your name and prints a welcome message
 your_name = "Kamogelo"
 print("Welcome to python, "+ your_name)
 
 
+print("Hello Python")
+my_name = "Kamogelo"
+age = 21
+place = "Hospital View"
+print("My name is", my_name)
+print("I'm", age,"years old and i currently live in Kagiso, specifically at",place,"with my mom.")
+print("I'm going to work with you for some to become a self taugt software engineer")
 
-
+x,y,z = "Python" , "is" , "awesome"
+print(x,y,z)
 
 
